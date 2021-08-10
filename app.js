af39f9973c347a9e413cd8106cbb056c272ed454
r4weiss <85521952+r4weiss@users.noreply.github.com>
2021-08-10 00:36:47 -0400

    Completed version
    Modern design refresh, uses an external emoji button to pull up emoji panel.

--- app.js
+++ app.js
@@ -1,32 +1,11 @@
-//NEXT STEPS
-
-//Adding Features
-// store and render the emojis from local storage
-// if no items are in local storage when you press "Load", deliver an error message
-// TODO allow a user to save and render a bunch of emojis to different groups/containers
-// TODO Allow the user to increase or decrease the size of a particular emoji
-// TODO Allow the user to type out an emoji and have it rendered
-// TODO set a limit to how many emojis you can add, once reached, disable the add button
-
-//Refactoring the code
-// TODO Decrease the amount of code needed to run the base functions
-
-//Change the design
-// TODO Change the design to a modern style
-
 //VARIABLES
-
 let myEmojis = []
-const emojiContainer1 = document.getElementById("emoji-container-1")
-// const emojiContainer2 = document.getElementById("emoji-container-2")
-const emojiInput = document.getElementById("emoji-input")
+const saveBtn = document.getElementById("save-btn")
 const loadBtn = document.getElementById("load-btn")
-const pushBtn = document.getElementById("push-btn")
-const popBtn = document.getElementById("pop-btn")
 const clearBtn = document.getElementById("clear-btn")
-const errorMsg = document.getElementById("error-msg")
-
+const msg = document.getElementById("msg")
 const emojiBtn = document.getElementById("emoji-btn")
+const emojiInput = document.getElementById("emoji-input")
 const picker = new EmojiButton()
 
 emojiBtn.addEventListener("click", () => {
@@ -37,86 +16,36 @@ picker.on("emoji", (emoji) => {
 	document.querySelector("input").value += emoji
 })
 
-//BUTTONS
-
 loadBtn.addEventListener("click", () => {
 	const emojisFromLocalStorage = JSON.parse(localStorage.getItem("myEmojis"))
 	if (emojisFromLocalStorage) {
 		myEmojis = emojisFromLocalStorage
-		renderEmojis()
+		emojiInput.value = myEmojis
+		msg.innerHTML = "Successfully loaded! 🚚"
 	} else {
-		errorMsg.innerHTML = "No emojis found 🔍"
+		msg.innerHTML = "🔎 We could not find any emojis! 🔍 "
 	}
 })
 
-pushBtn.addEventListener("click", function () {
+saveBtn.addEventListener("click", function () {
 	if (emojiInput.value) {
-		myEmojis.push(emojiInput.value)
+		myEmojis = emojiInput.value
 		localStorage.setItem("myEmojis", JSON.stringify(myEmojis))
-		errorMsg.innerHTML = ""
-		renderEmojis()
+		emojiInput.value = ""
+		msg.innerHTML = "Successfully saved! 🥳"
+	} else {
+		msg.innerHTML = "👉 It takes one to save one! 🤓"
 	}
-})
-
-popBtn.addEventListener("click", function () {
-	myEmojis.pop()
-	localStorage.setItem("myEmojis", JSON.stringify(myEmojis))
-	renderEmojis()
 })
 
 clearBtn.addEventListener("click", function () {
 	myEmojis = []
 	localStorage.removeItem("myEmojis")
-	renderEmojis()
+	emojiInput.value = ""
+	msg.innerHTML =
+		"💀 All emojis have been eliminated with extreme prejudice! 💀"
 })
 
-//FUNCTIONS
-
-function renderEmojis() {
-	if (myEmojis) {
-		let emojiSpan = ""
-		for (let i = 0; i < myEmojis.length; i++) {
-			emojiSpan += `<span>${myEmojis[i]}</span>`
-		}
-		emojiContainer1.innerHTML = emojiSpan
-		// emojiContainer2.innerHTML = emojiSpan
-		emojiInput.value = ""
-	}
+if (emojiInput.value === "") {
+	msg.innerHTML = ""
 }
-
-//Make a function that does the following:
-
-// Listens if you want to add and what you want to add, then adds and renders
-// Listens if you want to remove, then removes and renders
-// Listens if you want to clear all, then clears all and renders
-
-function render(action, emoji) {
-	const emojisFromLocalStorage = JSON.parse(localStorage.getItem("myEmojis"))
-	// Listens if you want to load, then loads and renders
-	if (emojisFromLocalStorage) {
-		myEmojis = emojisFromLocalStorage
-		renderEmojis()
-	} else {
-		errorMsg.innerHTML = "No emojis found 🔍"
-	}
-
-	if (myEmojis) {
-		let emojiSpan = ""
-		for (let i = 0; i < myEmojis.length; i++) {
-			emojiSpan += `<span>${myEmojis[i]}</span>`
-		}
-		emojiContainer.innerHTML = emojiSpan
-		emojiInput.value = ""
-	}
-}
-
-// import { EmojiButton } from "@joeattardi/emoji-button"
-
-// const picker = new EmojiButton()
-// const trigger = document.querySelector(".trigger")
-
-// picker.on("emoji", (selection) => {
-// 	trigger.innerHTML = selection.emoji
-// })
-
-// trigger.addEventListener("click", () => picker.togglePicker(trigger))
